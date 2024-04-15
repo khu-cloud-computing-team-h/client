8b2def125ed4c16b13a1369957795da0861c005e
Jeon Eonseok <jseokeon@gmail.com>
2024-04-15 19:36:13 +0900

    feat: 드롭박스 영역 구현

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,6 +1,39 @@
 import styled from '@emotion/styled';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@chakra-ui/react';
+import { useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log('file reading was aborted');
+      reader.onerror = () => console.log('file reading has failed');
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
+      )}
+    </div>
+  );
+}
 
 const Header = () => {
   const redirect_uri = import.meta.env.VITE_REDIRECT_URL;
@@ -22,27 +55,32 @@ const Header = () => {
   };
 
   return (
-    <HeaderWrapper>
-      <Button size='lg' colorScheme='blue'>
-        <Link to='/'>Home</Link>
-      </Button>
-      <Navigation>
-        {!ACCESS_TOKEN && (
-          <Button
-            size='lg'
-            colorScheme='green'
-            onClick={handleGoogleLoginButton}
-          >
-            로그인
-          </Button>
-        )}
-        {ACCESS_TOKEN && (
-          <Button size='lg' colorScheme='pink' onClick={handleSignOut}>
-            로그아웃
-          </Button>
-        )}
-      </Navigation>
-    </HeaderWrapper>
+    <>
+      <HeaderWrapper>
+        <Button size='lg' colorScheme='blue'>
+          <Link to='/'>Home</Link>
+        </Button>
+        <Navigation>
+          {!ACCESS_TOKEN && (
+            <Button
+              size='lg'
+              colorScheme='green'
+              onClick={handleGoogleLoginButton}
+            >
+              로그인
+            </Button>
+          )}
+          {ACCESS_TOKEN && (
+            <Button size='lg' colorScheme='pink' onClick={handleSignOut}>
+              로그아웃
+            </Button>
+          )}
+        </Navigation>
+      </HeaderWrapper>
+      <DropzoneWrapper>
+        <MyDropzone />
+      </DropzoneWrapper>
+    </>
   );
 };
 
@@ -69,4 +107,20 @@ const Navigation = styled.nav`
   display: flex;
   gap: 5rem;
   align-items: center;
+`;
+
+const DropzoneWrapper = styled.section`
+  width: 90%;
+  margin: 10rem auto;
+  cursor: pointer;
+  border: 5px dashed black;
+
+  p {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 20rem;
+    font-size: 2rem;
+  }
 `;
