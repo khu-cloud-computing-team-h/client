3fbe3004081aad856a0bb8cd68cad9e8162c6861
Jeon Eonseok <jseokeon@gmail.com>
2024-06-12 00:31:06 +0900

    chore: stale time 제거

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -21,7 +21,6 @@ export default function Dashboard() {
 
       return res.data;
     },
-    staleTime: 1000 * 60 * 60 * 60,
   });
 
   const { data: dataImage, isLoading: isImageDataLoading } = useQuery({
