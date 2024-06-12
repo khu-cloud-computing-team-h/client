829f22659f7819c4ef8e1efa6f197b009dc7fdea
Jeon Eonseok <jseokeon@gmail.com>
2024-06-13 02:40:53 +0900

    feat: 검색 기능 구현

--- src/Layout/Search.jsx
+++ src/Layout/Search.jsx
@@ -1,14 +1,28 @@
 import styled from '@emotion/styled';
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(e.target[0].value);
-};
+import instance from '../apis/instance';
 
 export default function Search() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const tags = e.target[0].value.replace(/\s/g, '').split(',');
+    const queryString = tags.map((tag) => `keywords=${tag}`).join('&');
+    console.log(queryString);
+
+    const res = await instance.get(`/search/tag?${queryString}`, {
+      params: {
+        size: 1000,
+      },
+    });
+
+    console.log(res.data.imageIds);
+  };
+
   return (
     <form onSubmit={handleSubmit}>
-      <Input type='text' placeholder='태그를 입력 후 엔터키를 눌러주세요' />
+      <Input
+        type='text'
+        placeholder='반드시 태그를 ,를 이용하여 구분해주세요. (ex. 만화, 몸짓, 미술). 입력이 완료되면 엔터키를 눌러주세요.'
+      />
     </form>
   );
 }
