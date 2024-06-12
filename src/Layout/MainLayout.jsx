2f1da96fa3b2f219f4dae4606b878a785226b3f2
Jeon Eonseok <jseokeon@gmail.com>
2024-06-13 00:51:37 +0900

    feat: dashboard에서만 검색 가능하도록 수정

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -2,12 +2,15 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import styled from '@emotion/styled';
 import Search from './Search';
+import { useParams } from 'react-router-dom';
 
 export default function MainLayout() {
+  const { imageID } = useParams();
+
   return (
     <>
       <Header />
-      <Search />
+      {!imageID && <Search />}
       <Main>
         <Outlet />
       </Main>
