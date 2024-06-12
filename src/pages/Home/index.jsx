8aa9fc3a15f5e8153fc477d8635479efba574d78
Jeon Eonseok <jseokeon@gmail.com>
2024-06-12 23:54:41 +0900

    feat: login 되어 있는 경우 바로 dashboard로 이동

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
+  const navigate = useNavigate();
+  const accessToken = localStorage.getItem('ACCESS_TOKEN');
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate('/dashboard');
+    }
+  }, [navigate, accessToken]);
   return <h1>Welcome</h1>;
 }
